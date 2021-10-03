17b45b7fb65354586d9b492b54fb5f6d0a00f432
v3f6x65nMeEnhUug <v3f6x65nMeEnhUug@gmail.com>
2021-10-03 15:17:54 -0400

    Added lazy update method

--- src/main/java/edu/brown/cs/student/database/Database.java
+++ src/main/java/edu/brown/cs/student/database/Database.java
@@ -295,4 +295,26 @@ public class Database {
     }
   }
 
+  public <T extends DBRelation> void update(T item, String columnName, String newValue) {
+    this.delete(item);
+    Class<? extends DBRelation> relationClass = item.getClass();
+    HashMap<String, Method> setterHashMap = new HashMap<String, Method>();
+    Method[] relationMethods = relationClass.getDeclaredMethods();
+    Stream<Method> methodStream = Arrays.stream(relationMethods);
+    Method[] relationAttributeSetters = methodStream.filter((m) ->
+            m.isAnnotationPresent(RelationAttributeSetter.class)).toArray(Method[]::new);
+
+    for (Method setter : relationAttributeSetters) {
+      String attributeName = setter.getAnnotation(RelationAttributeSetter.class).name();
+      setterHashMap.put(attributeName, setter);
+    }
+    try {
+      setterHashMap.get(columnName).invoke(item, newValue);
+    } catch (IllegalAccessException e) {
+      e.printStackTrace();
+    } catch (InvocationTargetException e) {
+      e.printStackTrace();
+    }
+    this.insert(item);
+  }
 }
