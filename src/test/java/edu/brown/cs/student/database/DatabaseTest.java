91bb8ff72eed7796a543ebaee163cb6bc385f6a8
bqbnakasprv7aqe6e <90489496+bqbnakasprv7aqe6e@users.noreply.github.com>
2021-10-02 23:55:17 -0400

    test: insert

--- src/test/java/edu/brown/cs/student/database/DatabaseTest.java
+++ src/test/java/edu/brown/cs/student/database/DatabaseTest.java
@@ -17,30 +17,63 @@ public class DatabaseTest {
 			db.addRelation(Review.class);
 			List<Review> queryResult = db.where("review_text == \"Hello world!\"", "reviews");
       for (Review r : queryResult) {
-        assertEquals(r.getId(), 1);
+        assertEquals(r.getID(), 1);
       }
 		} catch (Exception e) {
       e.printStackTrace();
-    }
+		}
 	}
 
 	@Test 
-	public void twoRelationTypesTest () {
+	public void twoRelationTypesTest() {
 		try {
 			Database db = new Database("./data/project-1/testDB1.sqlite3");
 			db.addRelation(Review.class);
       db.addRelation(User.class);
 			List<Review> queryResult = db.where("review_text == \"Hello world!\"", "reviews");
       for (Review r : queryResult) {
-        assertEquals(r.getId(), 1);
+        assertEquals(r.getID(), 1);
       }
 			List<User> queryResult2 = db.where("user_id == \"1\"", "users");
-      for (User r : queryResult2) {
-        assertEquals(r.getUserID(), "1");
+      for (User u : queryResult2) {
+        assertEquals(u.getUserID(), "1");
       }
 		} catch (Exception e) {
       e.printStackTrace();
-    }
+		}
 	}
+
+	@Test
+	public void testInsert() {
+		try {
+			Database db = new Database("./data/project-1/testDB1.sqlite3");
+			db.addRelation(Review.class);
+			db.addRelation(User.class);
+
+			Review rave = new Review();
+			rave.setReviewText("mind-blowing, life-changing, never-to-be-forgotten");
+			rave.setReviewSummary("very pleased");
+			rave.setReviewDate("10.03.2021");
+			rave.setID(4);
+
+			db.insert(rave);
+
+			List<Review> queryResult = db.where("review_text == \"mind-blowing, " +
+					"life-changing, never-to-be-forgotten\"", "reviews");
+
+			for (Review r : queryResult) {
+				assertEquals(r.getID(), 4);
+			}
+
+			List<Review> queryResult2 = db.where("review_date == \"10.03.2021\"", "reviews");
+
+			for (Review r : queryResult2) {
+				assertEquals(r.getID(), 4);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 
 }
